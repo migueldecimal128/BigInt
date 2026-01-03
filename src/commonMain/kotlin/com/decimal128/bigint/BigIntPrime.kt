66b32b109ee192679add383eb35a6dbbbaf129b2
MiguelDecimal128 <miguel@decimal128.com>
2026-01-03 16:45:27 -0500

    increase size of SMALL_PRIMES, while still keeping relatively simple.

--- src/commonMain/kotlin/com/decimal128/bigint/BigIntPrime.kt
+++ src/commonMain/kotlin/com/decimal128/bigint/BigIntPrime.kt
@@ -79,7 +79,9 @@ object BigIntPrime {
             }
         }
     }
-
+    // TODO - there are multiple ways to pack this if need be. A ByteArray
+    //  of deltas would cut the size in half, yet still be easy to handle.
+    //  A bitmap would be harder to handle, but smaller.
     private val SMALL_PRIMES = shortArrayOf(
         3, 5, 7, 11, 13, 17, 19, 23,
         29, 31, 37, 41, 43, 47, 53, 59,
@@ -89,7 +91,14 @@ object BigIntPrime {
         181, 191, 193, 197, 199, 211, 223, 227,
         229, 233, 239, 241, 251, 257, 263, 269,
         271, 277, 281, 283, 293, 307, 311, 313,
-        317
+        317, 331, 337, 347, 349, 353, 359, 367,
+        373, 379, 383, 389, 397, 401, 409, 419,
+        421, 431, 433, 439, 443, 449, 457, 461,
+        463, 467, 479, 487, 491, 499, 503, 509,
+        521, 523, 541, 547, 557, 563, 569, 571,
+        577, 587, 593, 599, 601, 607, 613, 617,
+        619, 631, 641, 643, 647, 653, 659, 661,
+        673, 677, 683, 691, 701, 709, 719, 727
     )
 
     private enum class SmallPrimeResult {
